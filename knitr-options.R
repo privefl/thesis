324f11c83455df4a8e7649417ae23f2b50de9f56
Florian Privé <florian.prive@inp-grenoble.org>
2018-10-31 13:17:13 +0100

    init

---
+++ knitr-options.R
@@ -0,0 +1,11 @@
+knitr::opts_chunk$set(
+  comment = "#>",
+  out.width = "70%", 
+  fig.align = 'center',
+  dev = "svg"
+)
+
+options(width = 90)
+
+Sys.setenv(LANGUAGE = "en")
+Sys.setlocale("LC_TIME", "C")
---
+++ spelling-check.R
@@ -0,0 +1,16 @@
+if (!exists("WORDS_TO_IGNORE")) WORDS_TO_IGNORE <- character(0)
+
+# Spelling check
+spelling_errors <- spelling::spell_check_files(
+  path = `if`(interactive(), rstudioapi::getSourceEditorContext()$path,
+              knitr::current_input()),
+  ignore = c(scan("WORDLIST", ""), WORDS_TO_IGNORE),
+  lang = "en-US"
+)
+
+if (nrow(spelling_errors)) {
+  capture.output(print(spelling_errors), file = "spelling.errors")
+  stop("Check spelling! Check file 'spelling.errors'")
+}
+
+rm(spelling_errors)
